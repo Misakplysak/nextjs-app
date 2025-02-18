4ce6e7a57a5f2ee9d3400ba6dae0d821a8f9e3ba
Michal <149673267+Misakplysak@users.noreply.github.com>
2025-02-18 10:25:42 +0100

    Initial commit

---
+++ app/pages/index.tsx
@@ -0,0 +1,9 @@
+import MyComponent from "../components/MyComponent";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <MyComponent />
+    </div>
+  );
+}
