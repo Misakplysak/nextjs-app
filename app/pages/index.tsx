70c5b28a50153cfd84486ef6fffe50ea5510f196
Misakplysak <michalmacha40@gmail.com>
2025-03-19 20:59:36 +0100

    sql database connected: my version

--- app/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import MyComponent from "../components/MyComponent";
-
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200">
-      <MyComponent />
-    </div>
-  );
-}
